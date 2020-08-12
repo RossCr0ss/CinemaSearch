58813ca524ec85aec65bc0df5d7147b40bcc5d4b
RossCr0ss <sobechko.r@gmail.com>
2020-08-12 11:03:31 +0300

    done header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Header,
+    },
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+ul[class],
+ol[class] {
+  padding: 0;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+ul[class],
+ol[class],
+li,
+figure,
+figcaption,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+}
+
+ul[class],
+ol[class] {
+  list-style: none;
+}
+
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+article > * + * {
+  margin-top: 1em;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+@media (prefers-reduced-motion: reduce) {
+  * {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+    <header class="header">
+        <nav class="topnav">
+            <button type="button" class="btn" @click="showModal">
+                <img src="@/assets/magnifiying-glass.svg" class="search-icon" />
+            </button>
+
+            <modal v-show="isModalVisible" @close="closeModal" />
+        </nav>
+    </header>
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+export default {
+    name: 'Header',
+    components: {
+        Modal,
+    },
+    data() {
+        return {
+            isModalVisible: false,
+        }
+    },
+    methods: {
+        showModal() {
+            this.isModalVisible = true
+        },
+        closeModal() {
+            this.isModalVisible = false
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+.header {
+    background-color: #2b3380;
+    padding: 10px 0;
+
+    .topnav {
+        overflow: hidden;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        button {
+            float: right;
+            padding: 6px 10px;
+            margin-right: 16px;
+            background: transparent;
+            font-size: 17px;
+            border: none;
+            cursor: pointer;
+
+            .search-icon {
+                width: 14px;
+                height: 14px;
+            }
+        }
+
+        button:hover {
+            background: #ccc;
+        }
+    }
+}
+</style>
